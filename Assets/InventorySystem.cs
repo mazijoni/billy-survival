8135b18997ed574f6f0d7d18325a9a14071c135f
mazijoni <132272609+mazijoni@users.noreply.github.com>
2023-05-01 16:25:33 +0200

    Add assets, step 10

---
+++ Assets/InventorySystem.cs
@@ -0,0 +1,169 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventorySystem : MonoBehaviour
+{
+
+    public static InventorySystem Instance { get; set; }
+
+    public GameObject inventoryScreenUI;
+
+    public List<GameObject> slotlist = new List<GameObject>();
+
+    public List<string> itemList = new List<string>();
+
+    private GameObject itemToAdd;
+
+    private GameObject whatSlotToEquip;
+
+    public bool isOpen;
+
+    public bool isFull;
+
+
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            Instance = this;
+        }
+    }
+
+
+    void Start()
+    {
+        isFull = false;
+        isOpen = false;
+
+        PopulateSlotList();
+    }
+
+    private void PopulateSlotList()
+    {
+        foreach (Transform child in inventoryScreenUI.transform)
+        {
+            if (child.CompareTag("slot"))
+            {
+                slotlist.Add(child.gameObject);
+
+
+
+
+            }
+
+
+
+        }
+
+
+
+    }
+
+
+
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.Tab) && !isOpen)
+        {
+
+            Debug.Log("i is pressed");
+            inventoryScreenUI.SetActive(true);
+            Cursor.lockState = CursorLockMode.None;
+            isOpen = true;
+
+        }
+        else if (Input.GetKeyDown(KeyCode.Tab) && isOpen)
+        {
+            inventoryScreenUI.SetActive(false);
+            Cursor.lockState = CursorLockMode.Locked;
+            isOpen = false;
+        }
+    }
+
+
+    public void AddToInventory(string itemName)
+    {
+
+
+
+
+
+
+
+
+        whatSlotToEquip = FindNextEnptySlot();
+        itemToAdd = (GameObject)Instantiate(Resources.Load<GameObject>(itemName), whatSlotToEquip.transform.position, whatSlotToEquip.transform.rotation);
+        itemToAdd.transform.SetParent(whatSlotToEquip.transform);
+        itemList.Add(itemName);
+
+
+
+
+
+
+
+    }
+
+    private GameObject FindNextEnptySlot()
+    {
+
+        foreach (GameObject slot in slotlist)
+        {
+            if (slot.transform.childCount == 0)
+            {
+                return slot;
+
+
+            }
+
+
+
+
+        }
+
+
+        return new GameObject();
+
+    }
+
+
+
+    public bool CheckIfFull()
+    {
+
+        int counter = 0;
+
+        foreach (GameObject slot in slotlist)
+        {
+            if (slot.transform.childCount > 0)
+            {
+                counter += 1;
+            }
+
+        }
+
+        if (counter == 21)
+        {
+
+            return true;
+
+        }
+        else
+        {
+
+            return false;
+
+        }
+
+
+    }
+
+}
